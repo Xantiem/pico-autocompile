cd0f24622e14304c4803a6e4c16d75964cd27df0
Nicholas <35278540+Xantiem@users.noreply.github.com>
2025-02-08 23:12:28 +0100

    added change to account for github not adding empty folders

--- compile.sh
+++ compile.sh
@@ -2,6 +2,42 @@
 
 # fetch and initialise sdk
 
+if [ -d "sdk" ]; then
+    echo "sdk folder found"
+else
+    mkdir sdk
+fi
+
+cd sdk/
+if [ -d "pico-sdk" ]; then
+    echo "pico-sdk found"
+else
+    git clone https://github.com/raspberrypi/pico-sdk/
+fi
+cd pico-sdk/
+git init
+export PICO_SDK_PATH=$(pwd)
+echo "PICO_SDK_PATH path at $PICO_SDK_PATH"
+cd ../../
+git init
+
+# now compile
+
+cmake -B build && \
+make -j $(getconf _NPROCESSORS_ONLN) -C build 
+
+# -B build means cmake creates the build folder if needed
+# && means make is only run if cmake was successful
+# -j tells make how many cpu threads to use
+# $(getconf _NPROCESSORS_ONLN) is the number of threads yout pc has
+# -j $(getconf _NPROCESSORS_ONLN) tells make to use all your cores
+# -C build tells make what folder to build from/in
+
+# -DPICO_BOARD=pico_w builds for pico w, not pico
+#!/bin/bash   
+
+# fetch and initialise sdk
+
 cd sdk/
 if [ -d "pico-sdk" ]; then
     echo "pico-sdk found"
